07eebf80bc3b2888069ffbd38391b55bd9a8ba96
Jan Zwiener <jan.zwiener@gmail.com>
2011-10-12 21:41:58 +0200

    Rule of three for CConfig

--- src/Config.h
+++ src/Config.h
@@ -31,6 +31,14 @@ struct cvar_t
  *   if it is not already in the system. The new cvar_t is then set to the
  *   supplied default value (here 1024).
  *
+ *   Example cfg file:
+ *
+ *   <cfg>
+ *     width     1024
+ *     height    768
+ *     level     "bath/bath.lbsp"
+ *   </cfg>
+ *
  */
 
 class CConfig
@@ -39,21 +47,38 @@ public:
     CConfig(void);
     virtual ~CConfig(void);
 
-    // Add stuff to the config system
+    // Cleanup
+    void          Unload(); // be careful when calling this, make sure no one is using the cvar_ts anymore
+
+    // Parse and add stuff to the config system
     bool          AddFile(const std::string& file); // add every line from the file to the config system. e.g. "game.cfg"
     bool          AddLine(const std::string& line); // add a single line to the config system. e.g. "width 1024"
-    void          Unload();
 
-    //
-    cvar_t*       GetVar(const std::string& name, const std::string default_value, bool createnew=true);
-    cvar_t*       GetVarFloat(const std::string& name, const float default_value, bool createnew=true);
-    int           GetVarAsInt(const std::string& name, const int default_value, bool createnew=true);
-    std::string   GetVarAsStr(const std::string& name, const std::string& default_value, bool createnew=true);
+    // Access variables directly
+    // createnew: create a cvar_t, if there is no cvar_t with the variable name.
+    // this is normally what you want.
+    cvar_t*       GetVar(const std::string& name,
+                         const std::string default_value,
+                         bool createnew=true);
+    cvar_t*       GetVarFloat(const std::string& name,
+                              const float default_value,
+                              bool createnew=true);
+    // One time, static access
+    int           GetVarAsInt(const std::string& name,
+                              const int default_value,
+                              bool createnew=true);
+    std::string   GetVarAsStr(const std::string& name,
+                              const std::string& default_value,
+                              bool createnew=true);
 
 private:
     cvar_t*       AddVarFloat(const std::string& name, const float value);
     cvar_t*       AddVar(const std::string& name, const std::string& value);
 
     std::map<std::string, cvar_t*> m_var;
+
+    // Rule of three
+    CConfig(const CConfig&);
+    CConfig& operator=(const CConfig&);
 };
 
